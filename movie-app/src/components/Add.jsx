865025f0205c485e06983a3b7942a51a63c5578e
Nima Saghikalhori <nimasaghikalhori@gmail.com>
2024-11-29 20:35:28 +0100

    add-event-to-input

--- movie-app/src/components/Add.jsx
+++ movie-app/src/components/Add.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Add = () => {
+    const [query, setQuery] = useState(""); 
+
+    const onChange = e => {
+        e.preventDefault();
+        setQuery(e.target.value); 
+    }
+
     return (
-        <div>
-            <h1>Add</h1>
+        <div className='add-page'>
+            <div className="container">
+                <div className="add-content">
+                    <div className="input-wrapper"> 
+                        <input type="text" 
+                            placeholder='Search for a movie...'
+                            value={query}  
+                            onChange={onChange}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
