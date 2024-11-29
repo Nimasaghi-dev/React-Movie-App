a1a6c1092d203b94216807cce23f125a5c1e77e8
Nima Saghikalhori <nimasaghikalhori@gmail.com>
2024-11-29 20:13:06 +0100

    add-routes-in-app.jsx

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import WatchList from './components/WatchList';
+import Watched from './components/Watched';
+import Add from './components/Add';
+import './App.css';
 
 
 function App() {
 
   return (
     <>
-      <h1>Movie App</h1>
+      <Router>
+        <Header />
+
+        <Routes> {/* allows us to move around the app */}
+          <Route exact path="/" element={<WatchList />} />
+          <Route path="/watched" element={<Watched />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </Router>
     </>
   )
 }
